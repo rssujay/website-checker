456e5a845a6c45127670cdd4c0c6a78f9bfaac24
rssujay <rssujay97@gmail.com>
2020-06-21 03:36:34 +0800

    Implement checking process with file updates

--- website_checker.py
+++ website_checker.py
@@ -1,23 +1,28 @@
 '''
-This is a periodic scrip which checks
-websites for changes and logs to a log
-file.
+This is a periodic script which checks
+websites for changes and if any, sends emails to emails listed in the
+mailing list for that website.
 
 1) Reads url and mailing list
-2) Retrieves webpage as text, encodes/compresses into compact form
-3) If it doesn't match with previous entry:
-    4) Sends email to notify
+2) Retrieves webpage as text, hashes into compact form
+TODO: 3) If it doesn't match with previous entry:
+            4) Sends email to notify
 '''
 
 import csv
 import logging
 import threading
-import base64
+import hashlib
 import requests
+import queue
 
 # Constants
 INTERVAL_IN_SECONDS = 3600
 LOGGER_NAME= 'website_checker'
+
+# Initialization
+update_queue = queue.SimpleQueue()
+checkers = []
 
 # Logger configuration
 logging.basicConfig(
@@ -32,8 +37,14 @@ def check_row(url, previous_encoded, mailing_list):
     try:
         logger = logging.getLogger(LOGGER_NAME)
         webpage = requests.get(url)
-        logger.info(webpage)
+        webpage_hashed = hashlib.md5(webpage.text.encode()).hexdigest()
 
+        if webpage_hashed != previous_encoded:
+            logger.info("Webpage changed: %s", url)
+            previous_encoded = webpage_hashed
+
+            #TODO Send email at this point
+            
     except Exception as e:
         logger.error(
             "Error when checking row: url=%s, previous_encoded=%s, mailing_list=%s, exception=%s",
@@ -42,35 +53,52 @@ def check_row(url, previous_encoded, mailing_list):
             mailing_list,
             e,
         )
+    
+    finally:
+        update_queue.put((url, previous_encoded, ' '.join(mailing_list)))
 
 logger = logging.getLogger(LOGGER_NAME)
 logger.info('--------------------\nScript "website_checker" has started.')
-checkers = []
 
-with open('website_checker.csv', newline='') as csv_file:
+with open('website_checker.csv','r+', newline='') as csv_file:
     pages = csv.reader(csv_file)
     next(pages)
 
     for row in pages:
         try:
-            url = row[0]
-            mailing_list = row[2].split(' ')
-            encoded_string = row[1]
+            url, hashed_content, mailing_list = row
 
             if min(len(mailing_list), len(url)) == 0:
                 raise IOError
 
-            thread = threading.Thread(target=check_row, args=[url, encoded_string, mailing_list])
+            thread = threading.Thread(target=check_row, args=[url, hashed_content, mailing_list.split(' ')])
             thread.start()
             checkers.append(thread)
         
         except (IndexError, IOError, Exception) as e:
             logger.error("Row error: %s, row = %s,", e, row)
+            update_queue.put((url, hashed_content, ' '.join(mailing_list)))
     
-csv_file.close()
 
-for checker in checkers:
-    checker.join()
+    for checker in checkers:
+        checker.join()
+
+    logger.info('Updated file rewrite started. Clearing existing rows...')
+
+    # Clear csv file
+    csv_file.seek(0)
+    csv_file.truncate()
+
+    fieldnames = ['url', 'Hashed content', 'Mailing List']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    while not update_queue.empty():
+        url, hashed_content, mailing_list = update_queue.get()
+        writer.writerow({'url': url, 'Hashed content': hashed_content, 'Mailing List': mailing_list})
+
+logger.info('Updated file rewrite completed successfully')
+csv_file.close()
 
 logger.info('Script "website_checker" has finished.\n--------------------')
 
